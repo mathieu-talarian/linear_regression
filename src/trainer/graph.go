6a474f837e9998d394705b745c638ff3d7f9d849
Mathieu Moullec <mathieumoullec@MacBook-Pro-de-Mathieu.local>
2019-01-27 22:35:22 +0100

    This really should not take 19 minutes to build.

--- src/trainer/graph.go
+++ src/trainer/graph.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"image/color"
+	"log"
 	"mathmoul/linear_regression/src/common"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
@@ -18,23 +19,33 @@ func points(datas datas) plotter.XYs {
 	return pts
 }
 
-func normalized(datas datas, limits common.Limits, thetas common.Thetas) *plotter.Function {
-	return plotter.NewFunction(func(x float64) float64 {
-		normed := (x - limits.Min) / (limits.Max - limits.Min)
-		return thetas.Zero + (thetas.One * normed)
+func normalized(datas datas, thetas common.Thetas) *plotter.Function {
+	f := plotter.NewFunction(func(x float64) float64 {
+		return thetas.Zero + (thetas.One * x)
 	})
+	f.Color = color.RGBA{0, 255, 0, 255}
+	return f
 }
 
-func graph(datas datas, limits common.Limits, thetas common.Thetas) {
+func graph(datas datas, thetas common.Thetas) {
 	p, err := plot.New()
+	p.Add(plotter.NewGrid())
 	if err != nil {
 		panic(err)
 	}
 	p.X.Label.Text = "Km"
 	p.Y.Label.Text = "Price"
-	plotutil.AddLinePoints(p, points(datas))
-	p.Add(normalized(datas, limits, thetas))
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "datas.png"); err != nil {
+	s, err := plotter.NewScatter(points(datas))
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.GlyphStyle.Color = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	p.Add(s)
+	p.Legend.Add("Datas", s)
+	f := normalized(datas, thetas)
+	p.Add(f)
+	p.Legend.Add("Normalised", f)
+	if err := p.Save(5*vg.Inch, 5*vg.Inch, "datas.png"); err != nil {
 		panic(err)
 	}
 }
